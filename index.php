8a7c4c5940501dd14d6e0d141c942c4923d5a5f5
Deej Potter <deejpotter@gmail.com>
2020-08-28 23:07:57 +1000

    Set up the files

---
+++ index.php
@@ -0,0 +1,14 @@
+<?php include "templates/header.php"; ?>
+
+<h1>Simple Database App</h1>
+
+<ul>
+    <li>
+        <a href="create.php"><strong>Create</strong></a> - add a user
+    </li>
+    <li>
+        <a href="read.php"><strong>Read</strong></a> - find a user
+    </li>
+</ul>
+
+<?php include "templates/footer.php"; ?>
