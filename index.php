17f1ee125290bcec5a6bd5ebf6dfd997b504aaff
Deej Potter <deejpotter@gmail.com>
2020-08-29 22:20:43 +1000

    Started fixing CRUD files

--- index.php
+++ index.php
@@ -2,13 +2,28 @@
 
 <h1>Simple Database App</h1>
 
-<ul>
-    <li>
-        <a href="create.php"><strong>Create</strong></a> - add a user
-    </li>
-    <li>
-        <a href="read.php"><strong>Read</strong></a> - find a user
-    </li>
-</ul>
+<form method="post">
+    <h2>Create</h2>
+    <label for="firstname">First Name</label>
+    <input type="text" name="firstname" id="firstname">
+    <label for="email">email address</label>
+    <input type="email" name="email" id="email">
+</form>
+
+<form method="post">
+    <h2>Read</h2>
+</form>
+
+<form method="post">
+    <h2>Update</h2>
+    <label for="firstname">First Name</label>
+    <input type="text" name="firstname" id="firstname">
+    <label for="email">email address</label>
+    <input type="email" name="email" id="email">
+</form>
+
+<form method="post">
+    <h2>Delete</h2>
+</form>
 
 <?php include "templates/footer.php"; ?>
