24dc1d687944dfee226d5801648fd7877b88e2fc
Deej Potter <deejpotter@gmail.com>
2020-09-10 14:35:16 +1000

    Delete config.php

--- config.php
+++
@@ -1,9 +0,0 @@
-<?php
-$host       = "webhosting2004.is.cc";
-$username   = "deejpott_deejpotter";
-$password   = "Daniel1990";
-$dbname     = "deejpott_basicphpapp";
-$tableName = "users";
-$options    = array(
-                PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION
-              );
