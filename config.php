1ef5c280ed6b1198fbe6f5eb528a01e10a244b37
Deej Potter <deejpotter@gmail.com>
2020-09-10 14:34:04 +1000

    Finished everything. Updated the variables to match the host.

--- config.php
+++ config.php
@@ -1,9 +1,8 @@
 <?php
-
-$host       = "localhost";
-$username   = "root";
-$password   = "";
-$dbname     = "basicphpapp";
+$host       = "webhosting2004.is.cc";
+$username   = "deejpott_deejpotter";
+$password   = "Daniel1990";
+$dbname     = "deejpott_basicphpapp";
 $tableName = "users";
 $options    = array(
                 PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION
